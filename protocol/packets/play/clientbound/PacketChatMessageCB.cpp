bbf75d07454bedd55669148697bf9a90e93f976b
Indhi Rousseau <contact@thekinrar.fr>
2020-09-19 00:41:29 +0200

    Use real JSON in ChatMessageCB

--- protocol/packets/play/clientbound/PacketChatMessageCB.cpp
+++ protocol/packets/play/clientbound/PacketChatMessageCB.cpp
@@ -2,18 +2,19 @@
 // Created by thekinrar on 18/09/2020.
 //
 
+#include <nlohmann/json.hpp>
+
 #include "PacketChatMessageCB.h"
 
 PacketChatMessageCB::PacketChatMessageCB(const std::string &message) : message(message) {}
 
 std::vector<std::byte> PacketChatMessageCB::bytes() {
-    std::string json(R"({"text":")");
-    json += message;
-    json += "\"}";
+    nlohmann::json j;
+    j["text"] = message;
 
     std::vector<std::byte> array;
     PacketData::writeVarInt(0x0E, array);
-    PacketData::writeString(json, array);
+    PacketData::writeString(j.dump(), array);
     PacketData::writeByte(0, array);
     PacketData::writeLong(0, array);
     PacketData::writeLong(0, array);
