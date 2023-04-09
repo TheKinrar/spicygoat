af72fd10737da885a0c3f6b4e7c766f48188fccf
Amaury Rousseau <kinrar@kinrar.io>
2023-04-09 02:44:49 +0200

    CI: build AppImage

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 set -e
+build_dir="${1:-build}"
 cd data
-pip3 install -r requirements.txt
 python3 build.py
 cd ..
-mkdir -p build
-cd build
+mkdir -p $build_dir
+cd $build_dir
 cmake -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . -j$(nproc)
